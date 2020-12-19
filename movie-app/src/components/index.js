17afeed70d40aa5b72e549588cab1102c4d29d29
IvayloBorisov <ivaylo_borisov@abv.bg>
2020-12-19 16:22:59 +0200

    Implement Footer component

--- movie-app/src/components/index.js
+++ movie-app/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as ListItem } from './ListItem/ListItem';
 export { default as Navigation } from './Navigation/Navigation';
 export { default as Header } from './Header/Header';
+export { default as Footer } from './Footer/Footer';
