9e251e54f64d0df6da573f6e4079fbd7961980fc
IvayloBorisov <ivaylo_borisov@abv.bg>
2021-01-07 14:17:50 +0200

    Update pagination Button CSS for mobile devices

--- movie-app/src/components/Button/Button.js
+++ movie-app/src/components/Button/Button.js
@@ -3,7 +3,7 @@ import styles from "./Button.module.css";
 const Button = ({ click, name }) => {
   return (
       <button className={styles.btn} onClick={click} name={ name }>
-        {name === "next" ? "Next" : "Previous"}
+        {name === "next" ? "Next" : "Prev"}
       </button>
   );
 };
