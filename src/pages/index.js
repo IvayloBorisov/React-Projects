4030488088df66579ca2ea70faaadac65e731bdd
IvayloBorisov <ivaylo_borisov@abv.bg>
2021-01-21 13:21:23 +0200

    Delete NotFound component fron index file in pages folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,4 +4,3 @@ export { default as TopRatedPage } from './TopRated/TopRated';
 export { default as UpcomingPage } from './Upcoming/Upcoming';
 export { default as MovieDetailsPage } from './MovieDetails/MovieDetails';
 export { default as TvDetailsPage } from './TvDetails/TvDetails';
-export { default as NotFoundPage } from './NotFound/NotFound';
