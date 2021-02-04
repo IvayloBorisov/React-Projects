eb86f5c88faf0d521e8204be6736d9f15cddb968
IvayloBorisov <ivaylo_borisov@abv.bg>
2021-02-04 09:36:21 +0200

    Change Router file to AppRouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Routes from './Routes';
+import AppRouter from './AppRouter';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <AppRouter />
   </React.StrictMode>,
   document.getElementById('root')
 );
